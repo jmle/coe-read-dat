d180e76d500ca4f6094e3529f3a6d06494208ec2
Juan Manuel Leflet Estrada <ypsilon888@gmail.com>
2022-10-17 10:41:57 +0200

    Extract all sprites for a given SPR file - working for MOUSE.SPR

--- src/main/java/org/jmle/Main.java
+++ src/main/java/org/jmle/Main.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -34,86 +36,125 @@ public class Main {
     }
 
     private static void extractSpr(String sprFilePath, String palFilePath) throws Exception {
-        BmpImage bmp = new BmpImage();
-        bmp.image = createSprImage(sprFilePath, palFilePath);
-        FileOutputStream out = new FileOutputStream(String.format("%s.bmp", sprFilePath));
-        BmpWriter.write(out, bmp);
+        List<Rgb888Image> images = createSprImages(sprFilePath, palFilePath);
+        for (int i = 0; i < images.size(); i++) {
+            Rgb888Image image = images.get(i);
+            BmpImage bmp = new BmpImage();
+            bmp.image = image;
+            FileOutputStream out = new FileOutputStream(String.format("%s.%s.bmp", sprFilePath, i));
+            BmpWriter.write(out, bmp);
+        }
     }
 
-    private static Rgb888Image createSprImage(String sprFilePath, String palFilePath) throws Exception {
+    private static List<Rgb888Image> createSprImages(String sprFilePath, String palFilePath) throws Exception {
+        List<Rgb888Image> images = new ArrayList<>();
+
         byte[] sprBytes = getFileBytes(sprFilePath);
         byte[] palBytes = getFileBytes(palFilePath);
 
-        int firstSprOffset = (sprBytes[0] << 4) & 0xFF;
-        int firstSprSizeX = (sprBytes[firstSprOffset + 1] | sprBytes[firstSprOffset]) & 0xFF;
-        int firstSprSizeY = (sprBytes[firstSprOffset + 3] | sprBytes[firstSprOffset + 2]) & 0xFF;
-        int numBytes = firstSprSizeX * firstSprSizeY * 10;
-        byte[] sprite = new byte[numBytes];
+        List<Integer> offsets = extractOffsets(sprBytes);
+        for (Integer sprOffset: offsets) {
+            int sprWidth = (sprBytes[sprOffset + 1] | sprBytes[sprOffset]) & 0xFF;
+            int sprHeight = (sprBytes[sprOffset + 3] | sprBytes[sprOffset + 2]) & 0xFF;
+            byte[][] planes = new byte[4][(sprWidth / 2) * sprHeight];         // horizontal shrink caused by mode X
 
-        int i = firstSprSizeY;          // cx (size.y)
-        int j = firstSprOffset + 10;    // si (tracks position in original data)
-        int k = 0;                      // di (tracks position in new data)
-        int p = 4;                      // plane count (Mode X)
-        int offset = 16;
-        while (p > 0) {
-            while (i > 0) {
-                int b = sprBytes[j++];
+            int i = sprHeight;              // cx (size.y)
+            int j = sprOffset + 10;         // si (tracks position in original data)
+            int k = 0;                      // di (tracks position in new data)
+            int p = 0;                      // plane count (Mode X)
+            while (p < 4) {
+                byte[] plane = planes[p];
+                while (i > 0) {
+                    int b = sprBytes[j++];
 
-                if (b == 0) {                   // 0 == transparency
-                    k += offset;
-                    i--;
-                } else if (b > 0) {
-                    do {
-                        int reps = b;
-                        while (reps > 0) {      // rep movsb
-                            sprite[k++] = sprBytes[j++];
-                            reps--;
-                        }
-                        b = sprBytes[j++];
-                        b = ~b + 1;             // neg al
-                        if (b == 0) {
-                            break;
-                        }
-                        k += b;
-                        b = sprBytes[j++];
-                    } while (b != 0);
+                    if (b == 0) {                   // 0 == transparency
+                        k += (4 - (k % 4));
+                        i--;
+                    } else if (b > 0) {
+                        do {
+                            int reps = b;
+                            while (reps > 0) {      // rep movsb
+                                try {
+                                    plane[k++] = sprBytes[j++];
+                                } catch (ArrayIndexOutOfBoundsException e) {
+                                    System.out.println("jarl");
+                                }
+                                reps--;
+                            }
+                            b = sprBytes[j++];
+                            b = ~b + 1;             // neg al
+                            if (b == 0) {
+                                break;
+                            }
+                            k += b;
+                            b = sprBytes[j++];
+                        } while (b != 0);
 
-                    k += offset;                // byte is zero
-                    i--;
-                } else {
-                    b = ~b + 1;                 // neg al
-                    do {
-                        k += b;
-                        b = sprBytes[j++];
-                        int reps = b;
-                        while (reps > 0) {      // rep movsb
-                            sprite[k++] = sprBytes[j++];
-                            reps--;
-                        }
-                        b = sprBytes[j++];
-                        b = ~b + 1;             // neg al
-                    } while (b != 0);
+                        k += (k % 4 != 0) ? (4 - (k % 4)) : 0;
+                        i--;
+                    } else {
+                        b = ~b + 1;                 // neg al
+                        do {
+                            k += b;
+                            b = sprBytes[j++];
+                            int reps = b;
+                            while (reps > 0) {      // rep movsb
+                                try {
+                                    plane[k++] = sprBytes[j++];
+                                } catch (ArrayIndexOutOfBoundsException e) {
+                                    System.out.println("jarl");
+                                }
+                                reps--;
+                            }
+                            b = sprBytes[j++];
+                            b = ~b + 1;             // neg al
+                        } while (b != 0);
 
-                    k += offset;                // byte is zero
-                    i--;
+                        k += (k % 4 != 0) ? (4 - (k % 4)) : 0;      // prevents from adding 4 if k is "at the end of the plane"
+                        i--;
+                    }
+                }
+                i = sprHeight;                  // another full loop of size.y
+                p++;                                // "select next plane"
+                k = 0;                              // reset dest index (new plane)
+            }
+
+            byte[] sprite = mergePlanes(planes);
+            BufferedRgb888Image image = new BufferedRgb888Image(sprWidth, sprHeight);
+            for (int y = 0; y < sprHeight; y++) {
+                for (int x = 0; x < sprWidth; x++) {
+                    image.setRgb888Pixel(x, y, getColor(sprite[sprWidth * y + x], palBytes));
                 }
             }
-            i = firstSprSizeY;                  // another full loop of size.y
-            p--;                                // "select next plane"
-            k = 4 - p;                          // "next plane": set "write offset" for next plane
+
+            images.add(image);
         }
 
-        BufferedRgb888Image image = new BufferedRgb888Image(firstSprSizeX, firstSprSizeY);
-        for (int y = 0; y < firstSprSizeY; y++) {
-            for (int x = 0; x < firstSprSizeX; x++) {
-                image.setRgb888Pixel(x, y, getColor(sprite[firstSprSizeX * y + x], palBytes));
-            }
-        }
-
-        return image;
+        return images;
     }
 
+    private static List<Integer> extractOffsets(byte[] sprBytes) {
+        List<Integer> offsets = new ArrayList<>();
+        int i = 0;
+        int j = (sprBytes[i] * 0x10) & 0xFFFFFF;
+        while (j != 0) {
+            offsets.add(j);
+            i += 2;
+            j = (sprBytes[i] * 0x10) & 0xFFFFFF;
+        }
+        return offsets;
+    }
 
+    public static byte[] mergePlanes(byte[][] planes) {
+        byte[] sprite = new byte[planes[0].length * 4];
+        for (int p = 0; p < planes.length; p++) {
+            byte[] plane = planes[p];
+            for (int i = 0; i < plane.length; i++) {
+                sprite[i * 4 + p] = plane[i];
+            }
+        }
+        return sprite;
+    }
 
     private static byte[] getFileBytes(String inputFilePath) throws IOException {
         File inputFile = new File(inputFilePath);
@@ -145,14 +186,6 @@ public class Main {
         int g = palette[pi + 1];
         int b = palette[pi + 2];
         return (r << 18 | g << 10 | b << 2) & 0xFFFFFF;
-    }
-
-    private static int[] toIntArray(byte[] byteArray) {
-        int[] intArray = new int[byteArray.length];
-        for (int i = 0; i < byteArray.length; i++) {
-            intArray[i] = ((byteArray[i] & 0xFF) << 2) & 0xFF;
-        }
-        return intArray;
     }
 
     private static void printPalette(byte[] palette) {
